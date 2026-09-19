ca0cf1c0b739ddd6812aa9845a473cf5daf1cf1b
snayder <snayderxdd76@gmailcom>
2026-09-19 00:10:17 -0500

    Mostrar ruta de base de datos

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,13 @@
 const { Sequelize } = require('sequelize');
 const path = require('path');
 
+const databasePath = path.join(__dirname, '..', '..', 'database.sqlite');
+
+console.log('📁 BASE DE DATOS:', databasePath);
+
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: path.join(__dirname, '..', '..', 'database.sqlite'),
+  storage: databasePath,
   logging: false,
 });
 
